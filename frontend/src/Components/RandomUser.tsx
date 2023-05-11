7eed2fe9b080ebdea568336b91f850b145a3d98a
Thanos-Kozadinos <akozadinos@hotmail.com>
2023-05-11 18:50:00 +0200

    fix updating name

--- frontend/src/Components/RandomUser.tsx
+++ frontend/src/Components/RandomUser.tsx
@@ -6,33 +6,31 @@ import { IName, IRandomUser, getRandomUser } from '../api/api'
 
 const RandomUser:FC = () => {
     const [randUser, setRandUser] = useState<IRandomUser>();
-    const [randUserName, setRandUserName] = useState<string>();
+    const [randUserFirstName, setRandUserFirstName] = useState<string>();
 
     const getData = async () => {
       const randomUserFromApi = await getRandomUser();
       setRandUser(randomUserFromApi);
-      setRandUserName(randUser?.results[0].name.first);
+      setRandUserFirstName(randomUserFromApi?.results[0].name.first);
     }
 
     useEffect(() => {
       getData();
-      setRandUserName(randUser?.results[0].name.first);
     },[]);
   
-    console.log(randUserName);
+
     return (
       <>
         <h1>Salt PGP Lab 1</h1>
         <form className='add-dev-form' >
             <input className='AddUserForm-name' onChange={(e) => {
-                setRandUserName(e.target.value);
-            }} placeholder='Enter user name' type="text" />
+                setRandUserFirstName(e.target.value);
+            }} placeholder='Enter user first name' type="text" />
             {/* <button className='AddUserForm-buton'>Add</button> */}
         </form>
         <div className="card"> 
-        <p>is there {randUserName}</p>    
             <p>
-                {randUser?.results[0].name.title} {randUser?.results[0].name.first} {randUserName} {randUser?.results[0].name.last}
+                {randUser?.results[0].name.title}  {randUserFirstName} {randUser?.results[0].name.last}
             </p>
             <p>
                 Age {randUser?.results[0].dob.age}
